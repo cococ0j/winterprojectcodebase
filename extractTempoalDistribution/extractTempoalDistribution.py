e50185e33358046740f0aad61eb71a1c829a1f61
cococ0j <asdjzh66@gmail.com>
2018-06-28 23:20:59 +1000

    1. Modifeid data structure of dates and cloudcovers; 2. Modified plot_cloudcover module to be consistent with the plot from the paper.

--- extractTempoalDistribution/extractTempoalDistribution.py
+++ extractTempoalDistribution/extractTempoalDistribution.py
@@ -10,21 +10,25 @@ The cloud covering is the 'Cloud_Coverage_Assessment' tag found in the xml file 
  <Cloud_Coverage_Assessment>86.49903571428571</Cloud_Coverage_Assessment>
 
 Sample commond:
-python extractTempoalDistribution.py -i /Users/jibusi/Downloads/winterproject/data/Bendigo2 -o /Users/jibusi/Downloads/winterproject/data
+python extractTempoalDistribution.py -i /Users/jibusi/Downloads/winterproject/data/Bendigo2 -o /Users/jibusi/Downloads/winterproject/data -p /Users/jibusi/Downloads/winterproject/data
 
 '''
 
 import os
+import sys
 import getopt
+import re
 import lxml
 from xml.etree.ElementTree import iterparse
-import sys
-from matplotlib import pyplot as plt
-#import matplotlib.dates as mdates
 import re
-#import numpy as np
+import numpy as np
+import pandas as pd
+from matplotlib import pyplot as plt
+import matplotlib.dates as mdates
 
 ############################################################################
+
+
 def permute(l, p):
     #assert(len(l) == len(p))
     pofl = [None] * len(l)  # p(l)
@@ -63,6 +67,8 @@ def parsexml(xmlfile):
     month = None
     day = None
     cloudcover = 0
+
+    # Use iterparse to save memory.
     for event, elem in iterparse(xmlfile):
 
         if elem.tag == "PRODUCT_START_TIME":
@@ -84,37 +90,38 @@ def parsexml(xmlfile):
 
 
 ############################################################################
-def plot_cloudcover(dates, cloudcovers):
+def plot_cloudcover(dates, cloudcovers, plotfile):
 
-    #dates = np.array(dates, dtype=np.datetime64)
+    dates = [pd.to_datetime(d, format='%Y%m%d', errors='ignore') for d in dates]
 
-    fig, ax = plt.subplots()
+    plt.ylim((-1, 100))  # set limit of y axis
+    plt.bar(dates, cloudcovers, color='orange', width=2.5, alpha=0.6)   # plot bar chart
+    plt.scatter(dates, cloudcovers, c='blue', alpha=1)    # plot scatter
 
-    plt.ylim((0, 100))
-    plt.bar(dates, cloudcovers)
-    plt.ticklabel_format(useOffset=False, style='plain')
-
-    # fig.autofmt_xdate()
-    #xfmt = mdates.DateFormatter('%d-%m-%y')
-    # ax.xaxis.set_major_formatter(xfmt)
-
+    # Customize ticks
+    plt.yticks(np.arange(0, 110, 10),
+               ('0 %', '10 %', '20 %', '30 %', '40 %', '50 %',
+                '60 %', '70 %', '80 %', '90 %', '100 %', '110 %'))
+    plt.xticks(rotation=45)
     plt.xlabel('Acquistion Dates')
     plt.ylabel('Cloud Covering')
+    plt.savefig(plotfile + '/' + 'figure.pdf', dpi=300, bbox_inches='tight')
     plt.show()
-
-
-inputfile = None
-outputfile = None
-
 
 
 ############################################################################
 #######################       Main fuction      ############################
 ############################################################################
 
+
+# Initialize
+inputfile = None
+outputfile = None
+plotfile = None
+
 # Assess parameters
 try:
-    opts, args = getopt.getopt(sys.argv[1:], "hi:o:", ["infile=", "outfile="])
+    opts, args = getopt.getopt(sys.argv[1:], "hi:o:p:", ["infile=", "outfile=", "plotfile="])
 except getopt.GetoptError:
     print 'Error: test_arg.py -i <inputfile> -o <outputfile>'
     print '   or: test_arg.py --infile=<inputfile> --outfile=<outputfile>'
@@ -133,13 +140,22 @@ for opt, arg in opts:
     elif opt in ("-o", "--outfile"):
         outputfile = arg
 
+    elif opt in ("-p", "--plotfile"):
+        plotfile = arg
+
 # Main process
 if outputfile != None and inputfile != None:
 
     folder_path = inputfile
+
+    # set default written destination of plot to be the same as the text file path
+    if plotfile == None:
+        plotfile = outputfile
+
     sub_folder_list = os.listdir(folder_path)
-    dates = []
-    cloudcovers = []
+
+    # In order to solve duplication of dates, we create a dictionary to store date, cloud covering pairs
+    d = {}
 
     # Each sub-folder represents a satellite product extracted from PEPS
     # In each sub-folder, we collect two information:
@@ -156,9 +172,21 @@ if outputfile != None and inputfile != None:
                 if re.match(r'.*(MTD).*(\.xml)$', file):
                     f = folder_path + "/" + sub_folder + "/" + file
                     date, cloudcover = parsexml(f)
-                    dates.append(date)
-                    cloudcovers.append(cloudcover)
+                    if date in d:
+                        d[date].append(cloudcover)
+                    else:
+                        d[date] = [cloudcover]
                     break
+
+    # Create date-cloud list
+    dates = []
+    cloudcovers = []
+
+    for date, cloudcover in d.iteritems():
+        dates.append(date)
+
+        # Take average of cloudcovers in the same day.
+        cloudcovers.append(np.mean(cloudcover))
 
     # Sort dates in ascending order.
     p = SortDatesPermutation(dates)
@@ -174,4 +202,7 @@ if outputfile != None and inputfile != None:
         output.close()
 
     # plot the cloud covering of the corresponding acquisition date
-    plot_cloudcover(dates_sorted, cloudcovers_sorted)
+    plot_cloudcover(dates_sorted, cloudcovers_sorted, plotfile)
+
+    print 'Dates file has been saved to ' + write_folder_path
+    print 'Plot has been saved to ' + plotfile + '/' + 'figure.pdf'
